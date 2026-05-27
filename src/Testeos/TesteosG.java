2bc3ac0bf1fca914d3339624698df7c22cfc13d9
WMasi-404 <maximolopez0808@gmail.com>
2026-05-27 14:41:55 -0300

    testeos

---
+++ src/Testeos/TesteosG.java
@@ -0,0 +1,13 @@
+package Testeos;
+import grafo.contenedores.*;
+import grafo.dirigido.*;
+import grafo.recursos.*;
+import archivos.*;
+
+
+public class TesteosG {
+
+    public static void main(String[] args) {
+        
+    }
+}
