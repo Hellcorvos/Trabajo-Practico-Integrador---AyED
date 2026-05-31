1f7dfcce01d40f9b6e81a1bc7856102f4cef7401
lunaaaapablo <lunapablo173@gmail.com>
2026-05-31 19:05:46 -0300

    organice el proyecto

---
+++ src/Testeos/TesteoInterfaz.java
@@ -0,0 +1,18 @@
+package Testeos;
+
+import javax.swing.SwingUtilities;
+
+import interfaz.SistemaAcademicoGUI;
+
+public class TesteoInterfaz {
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(() -> {
+
+            SistemaAcademicoGUI gui =
+                    new SistemaAcademicoGUI();
+
+            gui.setVisible(true);
+        });
+    }
+}
